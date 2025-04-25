9105602127cec49a98da35bf2e21a7638e7e0fdf
Thiago <sosat279@gmail.com>
2025-04-25 19:29:37 -0300

    Primer commit

---
+++ src/Ejercicio1.java
@@ -0,0 +1,20 @@
+public class Ejercicio1 {
+    public static void main(String[] arg){
+
+        System.out.println(parImpar(6));
+
+    }
+
+public static boolean parImpar(int num){
+
+    if(num % 2 == 0)
+    {
+        return true;
+    }else
+    {
+        return false;
+    }
+
+    }
+
+}

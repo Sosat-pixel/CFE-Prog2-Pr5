85e2c594e3c10678c29bd80501a48296eea77116
Thiago <sosat279@gmail.com>
2025-04-25 22:35:35 -0300

    Ejercicios 6 y 7 Modificaciones

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,7 +1,6 @@
 public class Ejercicio6 {
     public static void main(String[] arg){
         onetoN(1,6);
-
     }
 
     public static void onetoN (int i, int n){
--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -1,7 +1,6 @@
 public class Ejercicio7 {
     public static void main(String[] arg){
         onetoNsuma(0,1,5);
-
     }
 
     public static void onetoNsuma (int suma,int i, int n){
@@ -11,8 +10,8 @@ public class Ejercicio7 {
             return;
         }
         suma = suma + i;
-        System.out.println(suma);
         onetoNsuma(suma,i+1,n);
+        System.out.println(suma);
     }
 
 }
